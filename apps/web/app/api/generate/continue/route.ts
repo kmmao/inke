49342e3ca07301c0a4894f8fddbe1756b816dadc
allen <liukaimaomao@gmail.com>
2023-12-24 03:56:25 +0800

    更新为4

--- apps/web/app/api/generate/continue/route.ts
+++ apps/web/app/api/generate/continue/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request): Promise<Response> {
     openai.apiKey = getRandomElement(api_keys.split(",")) || api_key;
 
     const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo-16k",
+      model: "gpt-4-turbo",
       messages: [
         {
           role: "system",
